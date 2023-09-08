eb39c41767a5f562a55ecb58c1fce95f9acb4a46
joaocarlosa <joaocarloslima99@gmail.com>
2023-09-07 21:54:16 -0300

    Adiciona rotas update e send

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,10 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TelegramController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -26,6 +18,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/telegram/updates', [TelegramController::class, 'getUpdates']);
+    Route::post('/telegram/send', [TelegramController::class, 'sendMessage']);
 });
 
 require __DIR__.'/auth.php';
