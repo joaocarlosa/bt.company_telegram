a522f05b7dfe9c7d70e3510df5e257d85687e610
joaocarlosa <joaocarloslima99@gmail.com>
2023-09-08 20:00:20 -0300

    feat: funcionamento pleno da plataforma

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -1,10 +1,15 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
+use App\Models\Photo;
 
 class DashboardController extends Controller
 {
-    //
+    public function index()
+    {
+        $photos = Photo::all();
+        return view('dashboard', ['photos' => $photos]);
+    }
+    
 }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TelegramController;
+use App\Http\Controllers\DashboardController;
 
 
 
@@ -10,15 +11,23 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/dashboard', [DashboardController::class, 'index'])
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+
+//Route::get('/show-photos', 'TelegramController@showPhotos');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('/telegram/updates', [TelegramController::class, 'getUpdates']);
+    Route::get('/telegram/getupdates', [TelegramController::class, 'getUpdates']);
     Route::post('/telegram/send', [TelegramController::class, 'sendMessage']);
 });
 
