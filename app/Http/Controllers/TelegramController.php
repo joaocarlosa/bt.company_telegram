a87a221c39424ed662b613e9621f3214b6d70665
joaocarlosa <joaocarloslima99@gmail.com>
2023-09-08 17:20:11 -0300

    feat: implementra funcionalidades do telegram

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -1,9 +1,14 @@
 <?php
+
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Update;
+use App\Models\Message;
+use App\Models\Photo;
 use App\Services\TelegramService;
+use Illuminate\Support\Facades\DB;
 use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Storage;
 
 class TelegramController extends Controller
 {
@@ -17,61 +22,98 @@ class TelegramController extends Controller
     public function getUpdates()
     {
         $updates = $this->telegramService->getUpdates();
-        return response()->json($updates);
-    }
-
-    public function getFile($file_id)
-    {
-        $response = $this->client->get('getFile', [
-            'query' => [
-                'file_id' => $file_id,
-            ],
-        ]);
-    
-        return json_decode($response->getBody(), true);
-    }
-    
-    
-
-    public function sendMessage(Request $request)
-    {
-        $request->validate([
-            'chat_id' => 'required',
-            'message' => 'required',
-        ]);
-
-        $chatId = $request->input('chat_id');
-        $message = $request->input('message');
-
-        $response = $this->telegramService->sendMessage($chatId, $message);
-
-        return response()->json($response);
-    }
-
-    public function checkUpdates()
-    {
-        $updates = $this->telegramService->getUpdates();
-
-        foreach ($updates['result'] as $update) {
-            if (isset($update['message']['photo'])) {
-                $lastPhoto = end($update['message']['photo']);
-                $file_id = $lastPhoto['file_id'];
-                
-                $this->downloadPhoto($file_id);
+        $updates = json_decode(json_encode($updates), true);
+        
+        DB::beginTransaction();
+        try {
+            foreach ($updates['result'] as $update) {
+                $this->processUpdate($update);
             }
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollback();
+            return response()->json(['status' => 'error', 'message' => $e->getMessage()]);
+        }
+        
+        return response()->json(['status' => 'success', 'result' => $updates['result']]);
+    }
+    
+    private function processUpdate($update)
+    {
+        if (Update::where('update_id', $update['update_id'])->doesntExist()) {
+            $this->storeUpdate($update);
+            $this->storeMessage($update);
+            $this->storePhotos($update);
+        }
+    }
+    
+    private function storeUpdate($update)
+    {
+        Update::create(['update_id' => $update['update_id']]);
+    }
+    
+    private function storeMessage($update)
+    {
+        Message::forceCreate([
+            'message_id' => $update['message']['message_id'],
+            'from_id' => $update['message']['from']['id'],
+            'from_name' => $update['message']['from']['first_name'],
+            'chat_id' => $update['message']['chat']['id'],
+            'chat_name' => $update['message']['chat']['first_name'],
+            'text' => $update['message']['text'] ?? null,
+            'date' => $update['message']['date'],
+        ]);
+    }
+    
+    private function storePhotos($update)
+    {
+        if (isset($update['message']['photo'])) {
+
+            usort($update['message']['photo'], function($a, $b) {
+                return ($b['width'] * $b['height']) <=> ($a['width'] * $a['height']);
+            });
+
+            $largestPhoto = $update['message']['photo'][0];
+            $filePath = $this->downloadAndSavePhoto($largestPhoto['file_id'], $largestPhoto['width'], $largestPhoto['height']);
+
+            Photo::forceCreate([
+                'file_id' => $largestPhoto['file_id'],
+                'file_unique_id' => $largestPhoto['file_unique_id'],
+                'file_size' => $largestPhoto['file_size'],
+                'width' => $largestPhoto['width'],
+                'height' => $largestPhoto['height'],
+                'message_id' => $update['message']['message_id'],
+                'file_path' => $filePath
+            ]);
         }
     }
 
-    protected function downloadPhoto($file_id)
+    private function downloadAndSavePhoto($fileId, $width, $height)
     {
-        $fileDetails = $this->telegramService->getFile($file_id);
-        
-        // Construir a URL do arquivo
-        $file_path = $fileDetails['result']['file_path'];
-        $file_url = "https://api.telegram.org/file/bot{$this->telegramService->getBotToken()}/{$file_path}";
-        
-        // Baixar a foto
+        $botToken = env('TELEGRAM_BOT_TOKEN');
         $client = new Client();
-        $client->get($file_url, ['sink' => 'path/to/save/image.jpg']);
+
+        $response = $client->get("https://api.telegram.org/bot{$botToken}/getFile?file_id={$fileId}");
+        $response = json_decode((string) $response->getBody(), true);
+
+        $fileTelegramPath = $response['result']['file_path'];
+
+        $image = $client->get("https://api.telegram.org/file/bot{$botToken}/{$fileTelegramPath}")->getBody();
+
+        $filePath = "public/telegram_photos/{$fileId}_{$width}x{$height}.jpg";
+        Storage::put($filePath, $image);
+
+        return $filePath; 
     }
+
+    public function showPhotos()
+    {
+        $photos = Photo::all();
+
+        
+        return view('show-photos', ['photos' => $photos]);
+    }
+
+    
+
 }
